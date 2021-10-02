528dadfed30749d748945f13645915467626e256
zakariahere <zakariabouazza@fastmail.com>
2021-10-02 13:28:52 +0200

    init project snapshot

---
+++ src/test/java/com/example/external/ExternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.external;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
