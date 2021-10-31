bcbbe0d63b5ee85c122813bc621157874479e85e
BOUAZZA Zakaria <zakaria@mbp-de-bouazza.intern>
2021-10-31 11:03:51 +0100

    adding validation sb

--- src/main/java/com/example/external/CommonProperties.java
+++ src/main/java/com/example/external/CommonProperties.java
@@ -4,11 +4,18 @@ package com.example.external;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Component
 @ConfigurationProperties(prefix = "medium.common")
 @Data
+@Validated
 public class CommonProperties {
+
+    @NotBlank
     private String comment;
 }
